262ff2c3ed65d26f4894a0f9f4808295a0a59a65
olivierlama <olivier.lama@gmail.com>
2022-10-13 00:24:24 +0200

    update script npm

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from server" });
 });
 
 // All other GET requests not handled before will return our React app
